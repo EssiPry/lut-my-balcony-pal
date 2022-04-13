b6218fc2ff37a15094b29955bd2c56a20bf17cb2
Erika Horros-Wallin <erika.horros@gmail.com>
2022-04-13 18:34:48 +0300

    Add rightmenubar

--- my_balcony_pal/lib/main.dart
+++ my_balcony_pal/lib/main.dart
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.green,
+        canvasColor: Colors.grey[200]
       ),
       home: const MyHomePage(title: 'My Balcony Pal'),
     );
@@ -63,6 +64,13 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    const drawerIcons = [Icon(Icons.person_outline), 
+                        Icon(Icons.edit_note), 
+                        Icon(Icons.source_outlined), 
+                        Icon(Icons.insights), 
+                        Icon(Icons.settings),
+                        Icon(Icons.local_florist),
+                        Icon(Icons.help_outlined)];
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
@@ -75,41 +83,73 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: const Center(
+        child: Text('My Page!'),
+      ),
+    
+
+      //TÄÄ on oikea menu debugin alla piilossa
+      endDrawer: Drawer(
+        child: ListView (
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            ListTile(
+              leading: Icon(Icons.person_outline),
+              title: const Text('Name'),
+              
+              onTap: () {
+                Navigator.pop(context);
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            const Divider(
+            color: Colors.grey
+          ),
+            ListTile(
+              leading: Icon(Icons.edit_note),
+              title: const Text('Notes'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.source_outlined),
+              title: const Text('Data'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.insights),
+              title: const Text('Graphs'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: const Text('Settings'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.local_florist_outlined),
+              title: const Text('Gardening info'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+             ListTile(
+              leading: Icon(Icons.help_outline),
+              title: const Text('Help & Feedback'),
+              onTap: () {
+                Navigator.pop(context);
+              },
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
